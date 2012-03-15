c054ff5cee51b3b697822ae6e094ecb0ceabeb8a
William Sherer <bswilliams@gmail.com>
2012-03-15 14:53:23 -0500

    More revisions to topbar.sh

--- topbar.sh
+++ topbar.sh
@@ -18,8 +18,8 @@ Y_POS=0
 CRIT="#e3a100"
 BAR_FG="#1793d1"
 BAR_BG="#444444"
-DZEN_FG="#d9fdee"
-DZEN_FG2="#c5e0e1"  #is default from xmonad.hs
+DZEN_FG="#c5e0e1"
+DZEN_FG2="#48a0b8"  #is default from xmonad.hs
 DZEN_BG="#060203"
 COLOR_SEP=$DZEN_FG2
 FONT="Consolas:pixelsize=16:antialias=true"
@@ -51,9 +51,9 @@ CAL_CMD="sh ${HOME}/bin/dzencal.sh"
 
 
 printVolInfo() {
-    Perc=$(pacmd list-sinks | sed -n 's/volume:\s0:\s*\([0-9]\{1,3\}%\).*/\1/p' | tail -n1)
+    Perc=$(pacmd list-sinks | sed -n 's/\svolume:\s0:\s*\([0-9]\{1,3\}%\).*/\1/p' | tail -n1)
     Mute=$(pacmd list-sinks | grep "muted: yes")
-        echo -n "^fg($DZEN_FG2) ^ca(1,$VOL_MUTE_CMD)^ca(4,$VOL_UP_CMD)^ca(5,$VOL_DOWN_CMD)VOL^ca()^ca()^ca() "
+        echo -n "^fg() ^ca(1,$VOL_MUTE_CMD)^ca(4,$VOL_UP_CMD)^ca(5,$VOL_DOWN_CMD)VOL^ca()^ca()^ca() "
         if [[ $Mute != "" ]]; then
             echo -n "$(echo $Perc | gdbar -fg $CRIT -bg $BAR_BG -h $BAR_H -w $BIGBAR_W -s o -ss 1 -sw 2 -nonl) "
             echo -n "^fg()off"
@@ -69,32 +69,33 @@ printCPUInfo() {
     [[ $CPULoad1 -gt 60 ]] && CPULoad1="^fg($CRIT)$CPULoad1^fg()"
     [[ $CPULoad2 -gt 60 ]] && CPULoad2="^fg($CRIT)$CPULoad2^fg()"
     [[ $CPULoad3 -gt 60 ]] && CPULoad3="^fg($CRIT)$CPULoad3^fg()"
-    echo -n " ^fg($DZEN_FG2)CPU ^fg($BAR_FG)${CPULoad0}%^fg($DZEN_FG2)/^fg($BAR_FG)${CPULoad1}%%^fg($DZEN_FG2)/^fg($BAR_FG)${CPULoad1}%"
+    echo -n " ^fg()CPU ^fg($BAR_FG)${CPULoad0}%^fg()/^fg($BAR_FG)${CPULoad1}%^fg()/^fg($BAR_FG)${CPULoad2}%^fg()/^fg($BAR_FG)${CPULoad3}%"
     return
 }
 
 printTempInfo() {
-    CPUTemp=$(sensors | sed -n 's/CPU Temp[^0-9]*\([^(]*\).*/\1/p' | tr -d [:space:])
-    #add MB temp? MBTemp=$(sensors | sed -n 's/MB Temp[^0-9]*\([^(]*\).*/\1/p' | tr -d [:space:])
-    GPUTemp=$(sensors | grep -A 2 'radeon' | sed -n 's/.*+\(.*$\)/\1/p' | tr -d [:space:])
+    CPUTemp=$(sensors | sed -n 's/CPU Temp[^0-9]*\([0-9]\+\).*/\1/p')
+    MBDTemp=$(sensors | sed -n 's/MB Temp[^0-9]*\([0-9]\+\).*/\1/p')
+    GPUTemp=$(sensors | grep -A 2 'radeon' | sed -n 's/.*+\([0-9]\+\).*/\1/p')
     [[ $CPUTemp -gt 60 ]] && CPUTemp="^fg($CRIT)$CPUTemp^fg()"
-    [[ $GPUTemp -gt 60 ]] && GPUTemp="^fg($CRIT)$GPUTemp^fg()"
-    echo -n "^fg($DZEN_FG2)TEMP ^fg($BAR_FG)${CPUTemp}^fg($DZEN_FG2)/^fg($BAR_FG)${GPUTemp}"
+    [[ $MBDTemp -gt 65 ]] && MBDTemp="^fg($CRIT)$MBDTemp^fg()"
+    [[ $GPUTemp -gt 65 ]] && GPUTemp="^fg($CRIT)$GPUTemp^fg()"
+    echo -n "^fg()TEMP ^fg($BAR_FG)${CPUTemp}°^fg()/^fg($BAR_FG)${MBDTemp}°^fg()/^fg($BAR_FG)${GPUTemp}°"
     return
 }
 
 printMemInfo() {
     [[ $MemPerc -gt 70 ]] # HUH? && CPUTemp="^fg($CRIT)$MemPerc^fg()"
-    echo -n "^fg($DZEN_FG2)MEM ^fg($BAR_FG)${MemPerc}%"
+    echo -n "^fg()MEM ^fg($BAR_FG)${MemPerc}%"
     return
 }
 
 printDropBoxInfo() {
     DropboxON=$(ps -A | grep -c dropbox)
     if [[ $DropboxON == "0" ]]; then
-        echo -n "^fg($DZEN_FG2)^ca(1,$DROP_START_CMD)DROPBOX^ca() ^fg()Off"
+        echo -n "^fg()^ca(1,$DROP_START_CMD)DROPBOX^ca() ^fg()Off"
     else
-        echo -n "^fg($DZEN_FG2)^ca(1,$DROP_STOP_CMD)DROPBOX^ca() ^fg($CRIT)On"
+        echo -n "^fg()^ca(1,$DROP_STOP_CMD)DROPBOX^ca() ^fg($CRIT)On"
     fi
     return
 }
@@ -102,19 +103,19 @@ printDropBoxInfo() {
 printMpdInfo() {
     MPDON=$(ps -A | grep -c mpd)
         if [[ $MPDON == "0" ]]; then
-        echo -n "^fg($DZEN_FG2)^ca(1,mpd)MPD^ca() ^fg()Off"
+        echo -n "^fg()^ca(1,mpd)MPD^ca() ^fg()Off"
     else
         [[ $MpdRepeat == "On" ]] && MpdRepeat="^fg($CRIT)$MpdRepeat^fg()"
         [[ $MpdRandom == "On" ]] && MpdRandom="^fg($CRIT)$MpdRandom^fg()"
-        echo -n "^fg($DZEN_FG2)^ca(1,$MPD_REP_CMD)REPEAT^ca() ^fg()$MpdRepeat "
-        echo -n "^fg($DZEN_FG2)| ^ca(1,$MPD_RAND_CMD)RANDOM^ca() ^fg()$MpdRandom "
-        echo -n "^fg($DZEN_FG2)| ^ca(1,$MPD_TOGGLE_CMD)^ca(4,$MPD_NEXT_CMD)^ca(5,$MPD_PREV_CMD)MPD^ca()^ca()^ca() $MpdInfo"
+        echo -n "^fg()^ca(1,$MPD_REP_CMD)REPEAT^ca() ^fg()$MpdRepeat "
+        echo -n "^fg()| ^ca(1,$MPD_RAND_CMD)RANDOM^ca() ^fg()$MpdRandom "
+        echo -n "^fg()| ^ca(1,$MPD_TOGGLE_CMD)^ca(4,$MPD_NEXT_CMD)^ca(5,$MPD_PREV_CMD)MPD^ca()^ca()^ca() $MpdInfo"
     fi
     return
 }
 
 printDateInfo() {
-    echo -n "^ca(1,$CAL_CMD)^fg()$(date '+%Y^fg($DZEN_FG2).^fg()%m^fg($DZEN_FG2).^fg()%d^fg($DZEN_FG)/^fg($DZEN_FG2)%a ^fg($DZEN_FG)| ^fg()%H^fg($DZEN_FG2):^fg()%M^fg($DZEN_FG2):^fg()%S')^ca() "
+    echo -n "^ca(1,$CAL_CMD)^fg()$(date '+%Y^fg($COLOR_SEP).^fg()%m^fg($COLOR_SEP).^fg()%d/^fg($DZEN_FG2)%a ^fg($COLOR_SEP)| ^fg()%H^fg($COLOR_SEP):^fg()%M^fg($COLOR_SEP):^fg()%S')^ca() "
     return
 }
 
@@ -130,7 +131,7 @@ printLeft() {
         printDropBoxInfo
         printSpace
         printMpdInfo
-        echo -n " ^fg()>^fg($BAR_FG)>^fg($DZEN_FG2)>"
+        echo -n " ^fg()>^fg($BAR_FG)>^fg()>"
         echo
     done
     return
