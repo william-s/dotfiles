9096e33d2a16b2449ad34c1dece37d04fb76f450
William Sherer <bswilliams@gmail.com>
2013-03-25 23:55:54 -0500

    fix prompt so it vcs updates w/o reloading

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,22 +4,21 @@ autoload -Uz vcs_info
 zstyle ':vcs_info:*'              enable        git #hg bzr cvs svn
 zstyle ':vcs_info:*' formats 'on %F{m}%b%c%u%m%F{n}'
 zstyle ':vcs_info:*' actionformats "%b%c%u|%F{c}%a%f"
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b|%F{c}%r%f'
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:git*+set-message:*' hooks git-status
 zstyle ':vcs_info:*' stagedstr '%F{g}●%f'
 zstyle ':vcs_info:*' unstagedstr '%F{y}●%f'
 
 
-function vcs_info_wrapper() {
+function prompt_precmd() {
     vcs_info
-    [[ -n "$vcs_info_msg_0_" ]] && echo "${vcs_info_msg_0_}%F{n}"
-}
 
-
-function set_prompt() {
     local cmd_status prompt_name host_color
     cmd_status="%(?,%F{green}★%f,%B%F{magenta}χ%b%f)"
     prompt_name="%(!,%F{red},%F{cyan})%n%f"
+
+
     if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
         host_color=yellow
     else
@@ -28,8 +27,8 @@ function set_prompt() {
     
     PROMPT="${prompt_name}@%F{${host_color}}%m%f [%~]
 ${cmd_status} %B%(!,%F{red}#,%F{blue}λ)%f%b "
-    RPROMPT="$(vcs_info_wrapper)"
+    RPROMPT="${vcs_info_msg_0_}"
 
 }
 
-set_prompt
+add-zsh-hook precmd prompt_precmd
