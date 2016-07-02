6c1a726014eafeef289adb882792f89910e5de05
William Sherer <bswilliams@gmail.com>
2016-07-02 00:05:32 -0500

    reset emacs config to basic settings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,6 +3,16 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
+;;(set-frame-parameter (selected-frame) 'alpha '(<active> [<inactive>]))
+(set-frame-parameter (selected-frame) 'alpha '(85 75))
+(add-to-list 'default-frame-alist '(alpha 85 75))
 
+(set-frame-font "Consolas-13")
+
+(global-prettify-symbols-mode t)
+
+;;(when window-system
+;;  (global-hl-line-mode))
+ 
 ;; Mutt support.
 (setq auto-mode-alist (append '(("/tmp/mutt.*" . mail-mode)) auto-mode-alist))
